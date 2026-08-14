f8b6f807cfb4c6b893b8538f2c66d6c1ce19737a
md shahid <shahiddelhi989@gmail.com>
2026-08-14 12:25:36 +0530

    added chunk

--- genrate_from_text_to_text/project/rag_based_project/2.process_audio_to_text.py
+++ genrate_from_text_to_text/project/rag_based_project/2.process_audio_to_text.py
@@ -16,9 +16,25 @@ text_result=model.transcribe(
   language="hi"
 )
 
-file=os.open("json/1.first.json","w")
-json_result=json.loads(text_result)
-file.write(json_result)
-file.close()
+print(text_result)  # dicstinalry 
+print(type(text_result)) # dictinalry 
 
-print(text_result)
+# extract usefull information 
+
+segements=[]
+
+
+for chunk  in text_result["segments"]:
+  segements.append({"id":chunk["id"],"start":chunk["start"],"end":chunk["end"],"text":chunk["text"]})
+
+
+
+json_format=json.dumps({"text":text_result["text"],"segements":segements}, indent=4) # it will convert the python data type in to json 
+print(type(json_format))
+
+
+# now bcz we have a json data now we can save it in a file 
+file=open("json/1.first.json","w") #open the file in write mode 
+file.write(json_format) # we have wrote here 
+file.close()  # clsed the file
+
