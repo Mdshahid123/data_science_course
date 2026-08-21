47e4fedbae6af7892bfa8c92848c5ff1a4445e1a
md shahid <shahiddelhi989@gmail.com>
2026-08-21 12:53:31 +0530

    creating embedding vector

--- genrate_from_text_to_text/project/rag_based_project/i.py
+++ genrate_from_text_to_text/project/rag_based_project/i.py
@@ -1,14 +1,70 @@
-# list=[1,2,4,10]
-# count=1
-# for item in list:
+import os
+import json
+import requests
 
-#   if(count==3):
-#     break
+def give_all_files():
+ json_files=os.listdir("json")
+ #print(json_files) # it will give all the files name in list
+ return json_files
+
+def read_file_content(file_path):
+ #lets read the content of current json file
+ read_file=open(file_path,"r")
+ content=read_file.read()
+ return content
+
+
+def create_embedding(text):
+ api_endpoint="http://localhost:11434/api/embed"
+
+ payload={
+  "model":"bge-m3",
+  "input":text
+ }
+
+ headers={"content-type":"application/json"}
+
+ response=requests.post(api_endpoint,json=payload,headers=headers)
+ print(response)
+ actual_response=response.json()
+ return actual_response["embeddings"][0]
+
+ 
+
+ 
+
+
+ 
+count=1
+for json_file in give_all_files():
+  if count==2:
+   break
   
-#   print(f"{count}iteration:{item}")
-#   count=count+1
-# print("i am outside ther loop")
+# step 1=reading a content of current file
+  content=read_file_content(f"json/{json_file}")
+  #step2= parse the current json file
+  parsed_content=json.loads(content)
+  chunk=parsed_content["segements"][0]
+
+  
+  print(chunk)
+  #now we have to create a embedding 
+  embedding_vector=create_embedding(chunk["text"])
+  #now we have to save this embedding vector in json file for future processing 
+  file=open(f"json_embedding/{json_file}","w")
+  json_format=json.dumps(embedding_vector)
+  file.write(json_format)
+  
+
+  count =count+1
+  
+
+  
+
+  
 
 
-for item in range(1,1000,1):
-  print(item)
+
+
+  
+  
